b4968340eaa8fbfea9eee9e627571fdb17b52fa4
Artem Guzhov <67904355+ArtemGuzhov@users.noreply.github.com>
2022-04-07 22:59:55 +0800

    commit

--- index.s
+++ index.s
@@ -1,31 +1,47 @@
-.data
+.data /* Объявление сегмента данных */
 
+/* Переменная типа string */
 print_format:
 .string "%d \n"
 
+/* Переменная типа int(long) занимает 4ре байта */
 array:
         .long 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+	
 array_end:
+/* Конец массива */
 
+/* Объявляем сегмент кода */
 .text
+/* Программа начинается с функции main */
 .global main
+/* Объявляем, что main это функция */
 .type main, @function
 
+/* Описание main */
 main:
+	/* Кладем указателя (адрес) на начало массива в регистр %ebx */
 	movl $array, %ebx  	
 
 edit:
+	/* Кладем в eax значение ebx */
 	movl (%ebx), %eax 
-	shl $1, %eax  
+	/* Логический сдвиг влево на одну ячейку у значения eax*/
+	shl $1, %eax
+	/* Обновление значения на которое ссылается ebx  */
 	movl %eax, (%ebx) 
-
+	
+	/* Сравниваем адрес конца массива с адресом ebx */
 	cmpl $array_end, %ebx  
+	/* Если array_end === ebx, то массив проитерирован */
 	je new_array 
 
+	/* Переходим к след ячейке памяти */
 	addl $4, %ebx 
 
 	movl (%ebx), %eax
-	sar $1, %eax  
+	/* Арифметический сдвиг вправо */
+	sar $1, %eax
         movl %eax, (%ebx)
 
 
@@ -33,7 +49,8 @@ edit:
 	je new_array 
 
 	addl $4, %ebx
-
+	
+	/* Безусловный переход в начало */
 	jmp edit 
 
 
@@ -44,12 +61,18 @@ new_array:
 
 
 print_arr:
+	/* Кладем в стек значение в ebx */
 	push (%ebx)  
-	push $print_format 	
-	call printf  	
+	/* Кладем в стек переменную */
+	push $print_format 
+	/* Передаем управление системной функции printf - все что есть в стеке выведет на экран */
+	call printf
+	/* ESP - регистр отвечающий за стек */
 	addl $8, %esp 
-
+	
+	/* По регистру ebx идем к след значению массива */
 	addl $4, %ebx 
 	cmpl $array_end, %ebx 
 	jne print_arr 
+	/* Возвращаем управление себе и завершаем программу */
 	ret 
